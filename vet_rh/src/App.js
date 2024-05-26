a21787c3c0c11a514e89b170b65bd17659e03c6c
yurisavage <yuritec@gmail.com>
2024-05-26 19:04:37 -0300

    navegacao das telas

--- vet_rh/src/App.js
+++ vet_rh/src/App.js
@@ -1,13 +1,29 @@
-import react from 'react';
-import { HashRouter, Route, Routes } from 'react-router-dom';
+import react, { useEffect, useState } from 'react';
+import { HashRouter, Link, Route, Routes } from 'react-router-dom';
 import Home from './Home';
 import Cadastro from './Cadastro';
 import Consulta from './Consulta';
 import Editar from './Edicao';
 import Galeria from './Galeria';
-import { AppBar, Box, Card, CardMedia, Typography } from '@mui/material';
+import { AppBar, Box, Button, Card, CardMedia, Typography } from '@mui/material';
 
 function App() {
+
+  const [oculto, setOculto] = useState(true);
+
+  const handleClickHome = () => setOculto(true);
+  const handleClick = () => setOculto(false);
+  // const handleClickEdicao = () => setOculto(true);  
+  // const handleClick = () => setOculto(true);
+  
+
+  useEffect(() => {
+
+    console.log('click - ', oculto)
+
+  },[oculto])
+
+
   return (
     <div className="App">
       <AppBar sx={{ background: '#900950' }}>
@@ -22,6 +38,7 @@ function App() {
       </AppBar>
 
       <HashRouter>
+        {oculto && 
         <Typography sx={{ position: 'fixed', height: '9rem', width: '100%', top: '7rem', zIndex: 1 }}>
           <Box sx={{ display: 'flex', background: 'white', border: '0.2rem solid',
                       borderRadius: '20rem', width: '30rem', height: '30rem',
@@ -30,6 +47,7 @@ function App() {
               <Box sx={{ background: '#900950', borderRadius: '20rem', width: '10rem', height: '10rem',
                           ml: '-2rem', ":hover": { background: "#BD126C" }
                       }} 
+                    onClick={handleClick}
               >
                 <Typography sx={{ fontFamily: 'Cursive', color: 'white', fontWeight: 'bold', 
                                   ml: '2.5rem', paddingTop: '3.8rem', fontSize: '1.2rem' }} >
@@ -39,6 +57,7 @@ function App() {
               <Box sx={{ background: '#900950', borderRadius: '20rem', width: '10rem', height: '10rem',
                           mt: '8rem', ml: '-2rem', ":hover": { background: "#BD126C" }
                       }} 
+                      onClick={handleClick}
               >
                 <Typography sx={{ fontFamily: 'Cursive', color: 'white', fontWeight: 'bold', 
                                   ml: '3rem', paddingTop: '3.8rem', fontSize: '1.2rem' }} >
@@ -56,6 +75,7 @@ function App() {
               <Box sx={{ background: '#900950', borderRadius: '20rem', width: '10rem', height: '10rem',
                           ml: '2rem', ":hover": { background: "#BD126C" }
                       }} 
+                    onClick={handleClick}
               >
                 <Typography sx={{ fontFamily: 'Cursive', color: 'white', fontWeight: 'bold', 
                                   ml: '2.5rem', paddingTop: '3.8rem', fontSize: '1.2rem' }} >
@@ -65,6 +85,7 @@ function App() {
               <Box sx={{ background: '#900950', borderRadius: '20rem', width: '10rem', height: '10rem',
                           mt: '8rem', ml: '2rem', ":hover": { background: "#BD126C" }
                       }} 
+                    onClick={handleClick}
               >
                 <Typography sx={{ fontFamily: 'Cursive', color: 'white', fontWeight: 'bold', 
                                   ml: '3rem', paddingTop: '3.8rem', fontSize: '1.2rem' }} >
@@ -73,16 +94,30 @@ function App() {
               </Box>
             </div>
           </Box>          
-        </Typography>
+        </Typography> }
+
+        {!oculto && 
+          <Typography sx={{ background: '#900950', height: '9rem', position: 'fixed', width: '100%', top: '0.5rem', zIndex: 1 }}>
+            <Button component={Link} to="/" variant="outlined" 
+                  sx={{ mt: '6rem', ml: '4rem', borderColor:'#A6EEE6FF', 
+                        color:'#900950', background: 'white', fontWeight: 'bold', fontSize: '1rem',
+                        ":hover": { background: "#E7E1E4", color: '#BD126C' }
+                      }}
+                    onClick={handleClickHome}
+            >
+              Home
+            </Button>            
+          </Typography>
+        }
 
         <div>
           <Typography>
             <Routes>
-              <Route pat="/" exact element={<Home />}></Route>
-              <Route pat="/cadastro" exact element={<Cadastro />}></Route>
-              <Route pat="/consulta" exact element={<Consulta />}></Route>
-              <Route pat="/edicao" exact element={<Editar />}></Route>
-              <Route pat="/galeria" exact element={<Galeria />}></Route>
+              <Route path="/" exact element={<Home />}></Route>
+              <Route path="/cadastro" exact element={<Cadastro />}></Route>
+              <Route path="/consulta" exact element={<Consulta />}></Route>
+              <Route path="/edicao" exact element={<Editar />}></Route>
+              <Route path="/galeria" exact element={<Galeria />}></Route>
             </Routes>
           </Typography>
         </div>        
